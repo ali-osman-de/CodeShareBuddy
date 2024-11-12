07b72965ebb477916d4111399aced38177f51046
Ali Osman Demirkollu <72684098+ali-osman-de@users.noreply.github.com>
2024-11-12 16:28:59 +0300

    .

---
+++ src/components/LandingPageComponents/WhyCodeShare.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const WhyCodeShare = () => {
+    return (
+        <div>WhyCodeShare</div>
+    )
+}
+
+export default WhyCodeShare
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function LandingPage() {
+const LandingPage = () => {
   return (
     <div>LandingPage</div>
   )
