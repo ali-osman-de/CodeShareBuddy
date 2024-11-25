e5600c5a6bd8952cf15ef831768d1858e159d875
Ali Osman Demirkollu <72684098+ali-osman-de@users.noreply.github.com>
2024-11-25 19:44:09 +0300

    back-fixed

--- src/components/LandingPageComponents/Header.jsx
+++ src/components/LandingPageComponents/Header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import illustration from "../../assets/illustration.png"; // Import the local image
+import illustration from "../../assets/bg-ground.jpeg"; 
 import {
   Button,
   Card,
@@ -20,14 +20,14 @@ const Header = () => {
         <CardImg
           alt="Card image cap"
           src={illustration}
-          className="cardImage"
+          className="cardImage rounded-5"
           style={{
             width: 1000,
             height: 600,
           }}
         />
         <CardImgOverlay className="formCoordinats">
-          <Form className="p-3 rounded-4 shadow-lg" style={{ backgroundColor: "#edf6f9" }}>
+          <Form className="p-3 rounded-4 shadow-lg" style={{ backgroundColor: "#e0afa0" }}>
             <Row className="row-cols-lg-auto g-3 align-items-center">
               <Col>
                 <Label className="visually-hidden" for="exampleEmail">
@@ -42,7 +42,7 @@ const Header = () => {
                 />
               </Col>
               <Col>
-                <Button className="px-3" style={{ backgroundColor: "#3a86ff", border: "none" }}>Register!</Button>
+                <Button className="px-3" style={{ backgroundColor: "#f9f7f3", border: "none", color:"#0b090a" }}>Register!</Button>
               </Col>
             </Row>
           </Form>
