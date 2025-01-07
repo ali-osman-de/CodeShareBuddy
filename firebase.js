7945016f0ed1f546df452f5de616b92a1d370e5e
Ali Osman Demirkollu <72684098+ali-osman-de@users.noreply.github.com>
2025-01-07 14:29:19 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAdCpCGVlsQLvUaXJ9o2EWVNc8uRLljkVo",
-  authDomain: "codesharebody.firebaseapp.com",
-  projectId: "codesharebody",
-  storageBucket: "codesharebody.firebasestorage.app",
-  messagingSenderId: "450679181743",
-  appId: "1:450679181743:web:870656256761255a85df5a",
-  measurementId: "G-H3294B520E",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 const app = initializeApp(firebaseConfig);
